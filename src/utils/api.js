ecf77e50f038d1ede083c836ebadc718533b08fe
sebbmn <sebbmn@gmail.com>
2017-08-17 15:48:45 +0200

    chore: git test

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 export function fetchCategories (category = '') {
   category = category.trim()
-
+  return('on y est')
   //return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
     //.then((res) => res.json())
     //.then(({ hits }) => hits.map(({ recipe }) => recipe))
