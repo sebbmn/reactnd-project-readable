bbf83d47d97bd500d6d7d7e44c252f0e20d0868b
sebbmn <sebbmn@gmail.com>
2017-08-17 18:18:15 +0200

    feat: api getAll function

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import '../utils/api'
+import { getAll } from '../utils/api'
 
 class App extends Component {
+  state = {
+    posts : []
+  }
+  componentDidMount() {
+    getAll().then((posts) => {
+      if(!posts.error) {
+        this.setState({posts})
+      }     
+    })  
+}
   render() {
     return (
       <div className="App">
@@ -12,9 +22,15 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Here the api test for now....
-          {console.log()}
+          Here the api test for now...
+          
         </p>
+        {this.state.posts[0] && this.state.posts.map((post) => (
+          <div key={post.id}>
+            <h1 key={post.id}>{post.id}</h1>
+            <div key={post.id+post.id}>{post.body}</div>
+          </div>
+        ))}
       </div>
     );
   }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
-export function fetchCategories (category = '') {
-  category = category.trim()
-  return('on y est')
-  //return fetch(`https://api.edamam.com/search?q=${food}&app_id=${API_ID}&app_key=${APP_KEY}`)
-    //.then((res) => res.json())
-    //.then(({ hits }) => hits.map(({ recipe }) => recipe))
+const api = `http://localhost:5001/`
+
+export const getAll = () => {
+  return fetch(`${api}posts`, { headers: { 'Authorization': 'whatever-you-want' }})
+  .then(res => res.json())
+  //.then(data => data)
 }
