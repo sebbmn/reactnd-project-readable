0df41ac813f8f0cde375aafd4f8c556b6b0e6306
sebbmn <sebbmn@gmail.com>
2017-09-14 21:36:31 +0200

    feat: display message for deleted posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,21 +5,29 @@ import Content from './Content'
 
 class Post extends Component {
   render () {
-    const {match, posts} = this.props
+    const {match, contents, posts} = this.props
 
+    const content = contents.find(content => content.id === match.params.postId)
     const post = posts.find(p => p.id === match.params.postId)
 
     return (
       <div className='post'>
-        <h1>{post && post.title}</h1>
-          <Content contentId={match.params.postId} isPost={true}></Content>
-          <CommentsList postId={match.params.postId}></CommentsList>
+        {post && (!content.deleted ? (
+          <div>
+            <h1>{post && post.title}</h1>
+            <Content contentId={match.params.postId} isPost={true}></Content>
+            <CommentsList postId={match.params.postId}></CommentsList>
+        </div>
+        ) : (
+          <div>This post has been deleted</div>
+        ))}
       </div>
     )
   }
 }
-function mapStateToProps ({ posts }) {
+function mapStateToProps ({ contents, posts }) {
   return {
+    contents,
     posts
   }
 }
